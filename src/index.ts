7f7f3e4ea1de4b5a9662148836a78f2ae73a7f9c
Athena <1339521912@qq.com>
2019-10-16 10:07:19 +0800

    update demo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { AxiosRequestConfig } from './types/index'
 import xhr from './xhr';
+import { buildURL } from './helper/url'
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transfromURL(config)
+}
+function transfromURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 export default axios
