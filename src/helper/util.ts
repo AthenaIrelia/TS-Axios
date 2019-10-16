d0ef0f86fb66063f5f569fd2f8d44040c4f6973e
Athena <1339521912@qq.com>
2019-10-16 12:58:47 +0800

    解析body数据

--- src/helper/util.ts
+++ src/helper/util.ts
@@ -1,7 +1,11 @@
+const toString = Object.prototype.toString
 export function isDate(val: any): val is Date {
-  return Object.prototype.toString.call(val) === '[object Date]'
+  return toString.call(val) === '[object Date]'
 }
 
 export function isObject(val: any): val is Object {
   return val !== null && typeof val === 'object'
+}
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
