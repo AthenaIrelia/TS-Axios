87360b3b7ebc305bc96a155e18a422ff49e6f443
Athena <1339521912@qq.com>
2019-10-22 22:08:04 +0800

    update

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,7 +7,7 @@ export type Method = 'get' | 'GET'
   | 'patch' | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string;
+  url?: string;
   method?: Method;
   headers?: any;
   data?: any;
@@ -32,4 +32,26 @@ export interface AxiosError extends Error {
   request?: any
   response?: AxiosResponse
   isAxiosError: boolean
+}
+export interface Axios {
+  request(config: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  // 以下三个与上面三个多了data参数
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
+export interface AxiosInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise;
 }
